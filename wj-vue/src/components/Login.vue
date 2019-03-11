558cca5282ba510ac19991a127518bda8e234dd7
Evan <botofantares@gmail.com>
2019-03-11 19:14:55 +0800

    update:modify login page.

--- wj-vue/src/components/Login.vue
+++ wj-vue/src/components/Login.vue
@@ -24,8 +24,8 @@
     data () {
       return {
         rules: {
-          account: [{required: true, message: '请输入用户名', trigger: 'blur'}],
-          checkPass: [{required: true, message: '请输入密码', trigger: 'blur'}]
+          account: [{required: true, message: '请输入正确的用户名', trigger: 'blur'}],
+          checkPass: [{required: true, message: '请输入正确的密码', trigger: 'blur'}]
         },
         checked: true,
         loginForm: {
