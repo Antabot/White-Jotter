db4494f7fe75f765e96eaab721f645ad9818e3e6
Evan <botofantares@gmail.com>
2019-03-14 20:42:12 +0800

    update:rewrite BookCard.vue

--- wj-vue/src/components/library/BookCard.vue
+++ wj-vue/src/components/library/BookCard.vue
@@ -1,56 +1,76 @@
 <template>
-<div class="book">
+  <div>
+    <div style="margin-bottom: 10px;display: flex;justify-content: center;align-items: center">
+      <el-input
+        placeholder="默认展示部分书籍，可以通过书名搜索更多书籍..."
+        prefix-icon="el-icon-search"
+        size="small"
+        style="width: 400px;margin-right: 10px"
+        v-model="keywords">
+      </el-input>
+      <el-button size="small" type="primary" icon="el-icon-search" @click="searchClick">搜索</el-button>
+    </div>
+<el-card style="width: 135px;margin-bottom: 20px;height: 233px" class="book" bodyStyle="padding:10px" v-for="(item,index) in books" :key="item.id"
+         v-loading="cardLoading[index]">
   <div class="cover">
-    <a href="#nowhere">
-      <img src="https://img3.doubanio.com/view/subject/m/public/s29958602.jpg" alt="">
+    <a href="/details?id='{{item.id}}'">
+      <img :src="item.cover" alt="">
     </a>
   </div>
   <div class="info">
     <div class="title">
-      <a href="">{{book.title}}</a>
+      <a href="/details?id='{{item.id}}'">{{item.title}}</a>
     </div>
-    <div class="author">余秀华</div>
+    <div class="author">{{item.author}}</div>
   </div>
-</div>
+</el-card>
+  </div>
 </template>
 
 <script>
+//    import ElCard from "../../../node_modules/element-ui/packages/card/src/main.vue";
+
     export default {
       name: 'BookCard',
-      methods: {
-        loadEmps () {
-          var _this = this
-          this.$axios.get('/libraryindex/books').then(resp => {
-            if (resp && resp.status === 200) {
-              var data = resp.data
-              _this.books = data.books
-          }
-        })
-      }
+      mounted: function () {
+        this.loadBooks()
       },
-      data: function () {
-        return {
-          books: [],
-          book: {
-            cover: '',
-            title: '',
-            author: '',
-            link: ''
-          },
-          rules: {
-            cover: [{required: true, message: '必填:封面', trigger: 'blur'}],
-            title: [{required: true, message: '必填:标题', trigger: 'blur'}],
-            author: [{required: true, message: '必填:作者', trigger: 'blur'}]
+      methods: {
+        loadBooks () {
+          var _this = this
+          this.$axios.get('/libraryindex').then(resp => {
+            if (resp && resp.status === 200) {
+              _this.books = resp.data
+              var length = resp.data.length
+              _this.cardLoading = Array.apply(null, Array(length)).map(function (item, i) {
+                return false
+              })
+            }
+          })
+        }
+      },
+
+        data: function () {
+          return {
+            books: [],
+            cardLoading: [],
+            keywords: ''
+//            book: {
+//              cover: '',
+//              title: '',
+//              author: '',
+//              link: ''
+//            },
+//            rules: {
+//              cover: [{required: true, message: '必填:封面'}],
+//              title: [{required: true, message: '必填:标题'}],
+//              author: [{required: true, message: '必填:作者'}]
+//            }
           }
         }
       }
-    }
 </script>
 <style scoped>
-  .book {
-  width: 115px;
-  height: 220px;
-}
 
   .cover {
     width: 115px;
@@ -62,6 +82,7 @@
   img {
     width: 115px;
     height: 172px;
+    margin: 0 auto;
   }
 
   .title {
