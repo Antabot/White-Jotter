8b224f468aab01570481af53c79de00add02e9c8
Evan <botofantares@gmail.com>
2019-04-13 17:50:50 +0800

    Update: 修改部分代码

--- wj/src/main/java/com/gm/wj/controller/LibraryController.java
+++ wj/src/main/java/com/gm/wj/controller/LibraryController.java
@@ -3,12 +3,22 @@ package com.gm.wj.controller;
 import com.gm.wj.pojo.Book;
 import com.gm.wj.pojo.Search;
 import com.gm.wj.service.BookService;
+import com.sun.imageio.plugins.common.ImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.PixelGrabber;
+import java.awt.image.RenderedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class LibraryController {
@@ -42,7 +52,7 @@ public class LibraryController {
     }
 
     @GetMapping("/api/categories/{cid}/books")
-    public List<Book> listByCategory(@PathVariable("cid")int cid) throws Exception {
+    public List<Book> listByCategory(@PathVariable("cid") int cid) throws Exception {
         if (0 != cid) {
             return bookService.listByCategory(cid);
         } else {
@@ -51,8 +61,32 @@ public class LibraryController {
     }
 
     @PostMapping("api/covers")
-    public String coversUpload(HttpServletRequest request) throws Exception {
-        return "aha";
+    public void coversUpload(MultipartFile file, HttpServletRequest request) throws Exception {
+        String folder = "D:/workspace/img";
+        File imageFolder = new File(folder);
+        File f = new File(imageFolder, getRandomString(6) + ".jpg");
+        String filename = file.getName();
+        if (!f.getParentFile().exists())
+            f.getParentFile().mkdirs();
+        try {
+            file.transferTo(f);
+            System.out.println(f.getPath());
+            System.out.println("http://localhost:8443/img/books/" + f.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        System.out.println(file.getOriginalFilename());
+    }
+
+    public String getRandomString(int length) {
+        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
     }
 
 }
