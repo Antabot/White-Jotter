f7e355fdc064b642d16c0d148d93547eca37cc80
Evan <botofantares@gmail.com>
2019-04-14 17:30:11 +0800

    Update: 更新 README.md

--- wj-vue/src/components/Login.vue
+++ wj-vue/src/components/Login.vue
@@ -1,22 +1,22 @@
 <template>
   <body id="paper">
-  <el-form :rules="rules" class="login-container" label-position="left"
-           label-width="0px" v-loading="loading">
-    <h3 class="login_title">系统登录</h3>
-    <el-form-item prop="account">
-      <el-input type="text" v-model="loginForm.username"
-                auto-complete="off" placeholder="账号"></el-input>
-    </el-form-item>
-    <el-form-item prop="checkPass">
-      <el-input type="password" v-model="loginForm.password"
-                auto-complete="off" placeholder="密码"></el-input>
-    </el-form-item>
-    <el-checkbox class="login_remember" v-model="checked"
-                 label-position="left"><span style="color: #505458">记住密码</span></el-checkbox>
-    <el-form-item style="width: 100%">
-      <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
-    </el-form-item>
-  </el-form>
+    <el-form :rules="rules" class="login-container" label-position="left"
+             label-width="0px" v-loading="loading">
+      <h3 class="login_title">系统登录</h3>
+      <el-form-item prop="account">
+        <el-input type="text" v-model="loginForm.username"
+                  auto-complete="off" placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item prop="checkPass">
+        <el-input type="password" v-model="loginForm.password"
+                  auto-complete="off" placeholder="密码"></el-input>
+      </el-form-item>
+      <el-checkbox class="login_remember" v-model="checked"
+                   label-position="left"><span style="color: #505458">记住密码</span></el-checkbox>
+      <el-form-item style="width: 100%">
+        <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
   </body>
 </template>
 <script>
