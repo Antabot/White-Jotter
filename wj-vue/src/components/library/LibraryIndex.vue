fcf1edad2bf8a3ca05641b74cc524a5cd0e129c5
Evan <botofantares@gmail.com>
2019-04-16 21:12:27 +0800

    Daily update

--- wj-vue/src/components/library/LibraryIndex.vue
+++ wj-vue/src/components/library/LibraryIndex.vue
@@ -1,24 +1,22 @@
 <template>
   <el-container>
     <el-aside style="width: 200px;margin-top: 20px">
+      <switch></switch>
       <SideMenu @indexSelect="listByCategory" ref="sideMenu"></SideMenu>
     </el-aside>
     <el-main>
-        <books class="books-area" ref="booksArea"></books>
+      <books class="books-area" ref="booksArea"></books>
     </el-main>
   </el-container>
 </template>
 
 <script>
   import SideMenu from './SideMenu'
-  import Tag from './Tag'
   import Books from './Books'
-  import About from '../common/About'
-  import ImgUpload from './ImgUpload'
 
   export default {
     name: 'AppLibrary',
-    components: {Books, Tag, SideMenu, About, ImgUpload},
+    components: {Books, SideMenu},
     methods: {
       listByCategory () {
         var _this = this
---
+++ wj-vue/src/components/library/ViewSwitch.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-switch
+    v-model="value"
+    active-text="列表视图">
+  </el-switch>
+</template>
+
+<script>
+  export default {
+    name: 'Switch',
+    data () {
+      return {
+        value: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
