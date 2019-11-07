17ba6eef5598e72c46f2f5053ef86074ab13f4c5
Evan <botofantares@gmail.com>
2019-11-07 21:01:05 +0800

    fix: can't remember path before redirect

--- wj/wj.sql
+++ wj/wj.sql
@@ -10,10 +10,27 @@ Target Server Type    : MYSQL
 Target Server Version : 80015
 File Encoding         : 65001
 
-Date: 2019-11-05 21:33:28
+Date: 2019-11-07 12:27:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for artworks
+-- ----------------------------
+DROP TABLE IF EXISTS `artworks`;
+CREATE TABLE `artworks` (
+  `id` int(5) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of artworks
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for book
