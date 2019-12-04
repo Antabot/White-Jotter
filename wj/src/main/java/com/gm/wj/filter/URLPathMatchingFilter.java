a760d5bde764947558dd565a328f3b746a0ea105
Evan <botofantares@gmail.com>
2019-12-04 12:50:14 +0800

    fix: fix status change function

--- wj/src/main/java/com/gm/wj/filter/URLPathMatchingFilter.java
+++ wj/src/main/java/com/gm/wj/filter/URLPathMatchingFilter.java
@@ -6,9 +6,13 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.PathMatchingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Set;
 
 public class URLPathMatchingFilter extends PathMatchingFilter {
@@ -17,6 +21,14 @@ public class URLPathMatchingFilter extends PathMatchingFilter {
 
     @Override
     protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        if (HttpMethod.OPTIONS.toString().equals((httpServletRequest).getMethod())) {
+            httpServletResponse.setStatus(HttpStatus.NO_CONTENT.value());
+            return true;
+        }
+
         if (null==adminPermissionService) {
             adminPermissionService = SpringContextUtils.getContext().getBean(AdminPermissionService.class);
         }
