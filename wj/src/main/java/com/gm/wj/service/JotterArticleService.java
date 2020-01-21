c492f8eab9ef53f33065d608c4ca2470ad14a9b0
Evan <botofantares@gmail.com>
2020-01-21 21:45:37 +0800

    refactor: daily optimization

--- wj/src/main/java/com/gm/wj/service/JotterArticleService.java
+++ wj/src/main/java/com/gm/wj/service/JotterArticleService.java
@@ -5,7 +5,6 @@ import com.gm.wj.pojo.JotterArticle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
